e3e3009b7b6f686fbdb9d1dd5578d4464198a712
Onknown <lmaosaurus9897@gmail.com>
2024-09-13 00:43:24 +0200

    V 1.2 Server run

--- src/main/java/org/server/Main.java
+++ src/main/java/org/server/Main.java
@@ -3,6 +3,12 @@ package org.server;
 import httpserver.config.Configuration;
 import httpserver.config.ConfigurationManager;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 
 public class Main {
     public static void main(String[] args) {
@@ -13,5 +19,37 @@ public class Main {
 
         System.out.println(conf.getWebroot() + ": is webroot");
         System.out.println(conf.getPort() + ": is port");
+
+        try {
+            ServerSocket serverSocket = new ServerSocket(conf.getPort());
+            Socket socket = serverSocket.accept();
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            //TODO we write
+            String html = "<html>" +
+                    "<head>" +
+                        "<title>Simple Java HTTP Server</title>" +
+                    "</head>" +
+                    "<body>" +
+                        "<h1>Page is server using simple Java HTTP Server</h1>" +
+                    "</body>" +
+                    "</html>";
+
+            final String CRLF = "\n\r"; //13 and 10
+
+            String response =
+                    "HTTP/1.1 200 OK" + CRLF + "Content-Length: " + html.getBytes().length + CRLF + CRLF + html + CRLF + CRLF;
+
+            outputStream.write(response.getBytes());
+
+            inputStream.close();
+            outputStream.close();
+            socket.close();
+            serverSocket.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
